27d7ee27c6c7ae84b494052fa38c2a06a96a2005
Ryan Hanrahan <rhanrahan99@gmail.com>
2026-03-20 16:07:44 -0400

    added commands to remove user and role data from local storage when logging out (caught bug when performing other tests)

--- allies-connect/src/pages/admin.js
+++ allies-connect/src/pages/admin.js
@@ -1,6 +1,9 @@
-function Admin() {
+import { Col, Container, Form, Row } from "react-bootstrap";
+import "../App.css";
+
+function Admin({ user, setUser , role, setRole }) {
     return (
-        <div>
+        <div className="text-container mt-5">
             <h1>Admin Dashboard</h1>
             <p>Welcome to the admin dashboard! Here you can manage users, view reports, and update system settings.</p>
         </div>
